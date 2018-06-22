aaa8072672f00c6e378b2149cb38d2cbdcc2d517
bronaghan <bronaghan@marketplace.org>
2018-06-22 10:23:59 -0700

    remove test eggs

--- lambda/custom/easter_eggs.js
+++ lambda/custom/easter_eggs.js
@@ -1,21 +1,21 @@
 module.exports = [
-  {
-    title: 'Inflation',
-    author: 'Kai Ryssdal',
-    alts: ['price of money', 'cost of money'],
-    guid: 'easter-inflation',
-    date: "2018-05-24",
-    audio: {
-      url: "https://s3.amazonaws.com/alexa-marketplace-make-me-smart/test-explainers/Alexa+-+Ryssdal+Inflation+-+MIXLEV_alexa.mp3",
-    },
-  },
-  {
-    title: 'Original Intro',
-    author: 'Brian Ronaghan',
-    guid: 'easter-intro',
-    date: '2028-6-12',
-    audio: {
-      url: "https://s3.amazonaws.com/alexa-marketplace-make-me-smart/utils/Alexa+-+Ryssdal+-+Wood+-+Welcome+New+User+Message+-+MIXLEV_alexa.mp3"
-    }
-  }
+  // {
+  //   title: 'Inflation',
+  //   author: 'Kai Ryssdal',
+  //   alts: ['price of money', 'cost of money'],
+  //   guid: 'easter-inflation',
+  //   date: "2018-05-24",
+  //   audio: {
+  //     url: "https://s3.amazonaws.com/alexa-marketplace-make-me-smart/test-explainers/Alexa+-+Ryssdal+Inflation+-+MIXLEV_alexa.mp3",
+  //   },
+  // },
+  // {
+  //   title: 'Original Intro',
+  //   author: 'Brian Ronaghan',
+  //   guid: 'easter-intro',
+  //   date: '2028-6-12',
+  //   audio: {
+  //     url: "https://s3.amazonaws.com/alexa-marketplace-make-me-smart/utils/Alexa+-+Ryssdal+-+Wood+-+Welcome+New+User+Message+-+MIXLEV_alexa.mp3"
+  //   }
+  // }
 ];
