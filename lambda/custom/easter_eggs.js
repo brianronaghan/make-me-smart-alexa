ffca4b9a5946c40ce12d05a1b7b9fd13a6195d29
bronaghan <bronaghan@marketplace.org>
2018-06-26 16:10:19 -0700

    new ex, and easter

--- lambda/custom/easter_eggs.js
+++ lambda/custom/easter_eggs.js
@@ -1,14 +1,14 @@
 module.exports = [
-  // {
-  //   title: 'Inflation',
-  //   author: 'Kai Ryssdal',
-  //   alts: ['price of money', 'cost of money'],
-  //   guid: 'easter-inflation',
-  //   date: "2018-05-24",
-  //   audio: {
-  //     url: "https://s3.amazonaws.com/alexa-marketplace-make-me-smart/utils/Alexa+-+Ryssdal+-+Wood+-+Welcome+New+User+Message+-+MIXLEV_alexa.mp3",
-  //   },
-  // },
+  {
+    title: 'Leadership',
+    author: 'Kai Ryssdal',
+    alts: ['leadership', 'lead her ship', 'lead a ship', 'lead', 'leader', 'about leadership', 'lee to ship', 'lead to shift'],
+    guid: 'easter-leadership',
+    date: "2018-05-24",
+    audio: {
+      url: "https://s3.amazonaws.com/alexa-marketplace-make-me-smart/explainers/dave-kansas-kai-explainer-mix.mp3",
+    },
+  }
   // {
   //   title: 'Original Intro',
   //   author: 'Brian Ronaghan',
