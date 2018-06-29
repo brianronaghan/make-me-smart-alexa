108243d4abc3c4329cd04a03acd4f56af21a3e15
bronaghan <bronaghan@marketplace.org>
2018-06-28 17:24:35 -0700

    turn off easter

--- lambda/custom/easter_eggs.js
+++ lambda/custom/easter_eggs.js
@@ -1,14 +1,14 @@
 module.exports = [
-  {
-    title: 'Leadership',
-    author: 'Kai Ryssdal',
-    alts: ['leadership', 'lead her ship', 'lead a ship', 'lead', 'leader', 'about leadership', 'lee to ship', 'lead to shift'],
-    guid: 'easter-leadership',
-    date: "2018-05-24",
-    audio: {
-      url: "https://s3.amazonaws.com/alexa-marketplace-make-me-smart/explainers/dave-kansas-kai-explainer-mix.mp3",
-    },
-  }
+  // {
+  //   title: 'Leadership',
+  //   author: 'Kai Ryssdal',
+  //   alts: ['leadership', 'lead her ship', 'lead a ship', 'lead', 'leader', 'about leadership', 'lee to ship', 'lead to shift'],
+  //   guid: 'easter-leadership',
+  //   date: "2018-05-24",
+  //   audio: {
+  //     url: "https://s3.amazonaws.com/alexa-marketplace-make-me-smart/explainers/dave-kansas-kai-explainer-mix.mp3",
+  //   },
+  // }
   // {
   //   title: 'Original Intro',
   //   author: 'Brian Ronaghan',
