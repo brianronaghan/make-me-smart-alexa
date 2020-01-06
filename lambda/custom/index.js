7ce957bc4aed914440e27b9128d1087009cb6b90
bronaghan <bronaghan@marketplace.org>
2020-01-06 11:49:30 -0800

    change relative path for handlers due to new lambda env setting

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,13 +2,13 @@
 var Alexa = require("alexa-sdk");
 var config = require('./config');
 
-var startHandlers = require('handlers/startHandlers');
-var homePageHandlers = require('handlers/homePageHandlers.js')
-var playingExplainerHandlers = require('handlers/playingExplainerHandlers.js');
-var iteratingExplainerHandlers = require('handlers/iteratingExplainerHandlers.js');
-var requestHandlers = require('handlers/requestHandlers.js');
-var changeInfoHandlers = require('handlers/changeInfoHandlers.js')
-var unresolvedHandlers = require('handlers/unresolvedHandlers.js')
+var startHandlers = require('./handlers/startHandlers');
+var homePageHandlers = require('./handlers/homePageHandlers.js')
+var playingExplainerHandlers = require('./handlers/playingExplainerHandlers.js');
+var iteratingExplainerHandlers = require('./handlers/iteratingExplainerHandlers.js');
+var requestHandlers = require('./handlers/requestHandlers.js');
+var changeInfoHandlers = require('./handlers/changeInfoHandlers.js')
+var unresolvedHandlers = require('./handlers/unresolvedHandlers.js')
 
 exports.handler = function(event, context) {
     if (event && event.resources && event.resources.indexOf("arn:aws:events:us-east-1:881439228984:rule/keep-warm") >-1) {
