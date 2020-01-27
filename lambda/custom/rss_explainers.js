a3808449639810061d43f4080df5c4b918c573fd
bronaghan <bronaghan@marketplace.org>
2020-01-27 12:26:01 -0800

    turn legacy explainers object

--- lambda/custom/rss_explainers.js
+++ lambda/custom/rss_explainers.js
@@ -9,16 +9,24 @@ let parser = new Parser({
 } 
 );
  
+let legacy_explainers = require('./explainers.js');
+
+let legacy_dictionary = {};
 (async () => {
+  legacy_explainers.forEach((exp) => {
+    legacy_dictionary[exp.toLowerCase()] = exp;
+
+  });
  
   let feed = await parser.parseURL('http://paper-marketplace.test/feed/alexa/mms-explainers');
   console.log(feed.title);
- 
+  
   feed.items.forEach(item => {
     // console.log(item)
   });
   let exps = feed.items.map(item => {
       let audio = {}
+      console.log(item.title);
     item.audios.forEach(ai => {
         if (ai.$ && ai.$.url) {
             if (ai.$.expression == 'sample') {
@@ -28,16 +36,24 @@ let parser = new Parser({
             }
         }
     })
-    
-    console.log(audio)
+    let alts = [];
+    alts.push(item.title.toLowerCase())
+    item.title.split(' ').forEach((word) => alts.push(word.toLowerCase()))
+    console.log(legacy_explainers.length);
+    let legacy_record = legacy_explainers.find((exp) => {
+      return exp.title.toLowerCase() == item.title.toLowerCase()
+    })
+    console.log(legacy_record)
+
     return {
         title: item.title,
         author: item.author, 
         guid: item.guid, 
         date: item.isoDate,
         guid: item.guid,
-        audio
-
+        audio,
+        keywords: alts,
+        alts
     }
   })
  
