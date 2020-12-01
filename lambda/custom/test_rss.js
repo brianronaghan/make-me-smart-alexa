04626b21d46fd93e15da7e722c96e12f990fbedb
bronaghan <bronaghan@marketplace.org>
2020-12-01 14:31:25 -0800

    test guid

--- lambda/custom/test_rss.js
+++ lambda/custom/test_rss.js
@@ -4,6 +4,9 @@ var rss_explainers = require('./rss_explainers');
 (()=> {
   rss_explainers.getExplainers.call(this, (err, myExplainers) => {
     console.log(myExplainers.length);
+    myExplainers.forEach((ex)=>{
+      console.log(ex.guid);
+    })
     // console.log(myExplainers);
   });
 
