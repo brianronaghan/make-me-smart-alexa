8347e55a62deb206e47753ad18010681b0ec8fee
bronaghan <bronaghan@marketplace.org>
2020-12-02 16:07:13 -0800

    async play for playing handlers plus hard set at 100

--- lambda/custom/handlers/playingExplainerHandlers.js
+++ lambda/custom/handlers/playingExplainerHandlers.js
@@ -24,79 +24,34 @@ module.exports = Alexa.CreateStateHandler(config.states.PLAYING_EXPLAINER, {
     // set spot in indices
     var deviceId = util.getDeviceId.call(this);
     util.nullCheck.call(this, deviceId);
-    var slot = slot || this.event.request.intent.slots;
-    var boundThis = this;
-    let addOneBool = false;
-    if (source === 'HOME_AFTER_LAUNCH') {
-      addOneBool = true
-    }
-    var chosenExplainer = util.itemPicker(slot, util.liveExplainers(), 'title', 'topic', addOneBool);
-    console.log("CHOSEN EXPLAINER value:  ", chosenExplainer)
-    if (chosenExplainer === -1) {
-      console.log("OUT OF BOUNDS, but by number")
-      var theNumber;
-      if (slot.query && slot.query.value) {
-        theNumber = util.stripActions(slot.query.value);
-        delete slot.query.value;
-      } else if (slot.index && slot.index.value) {
-        theNumber = util.stripActions(slot.index.value);
-        delete slot.index.value;
-      } else if (slot.ordinal && slot.ordinal.value) {
-        theNumber = util.stripActions(slot.ordinal.value);
-        delete slot.ordinal.value;
-      }
-      var message = `${theNumber} is not a valid choice. Please choose between 1 and ${util.liveExplainers().length}. Let's try again. `
-      var boundThis = this;
-      return util.sendProgressive(
-        boundThis.event.context.System.apiEndpoint, // no need to add directives params
-        boundThis.event.request.requestId,
-        boundThis.event.context.System.apiAccessToken,
-        message,
-        function (err) {
-          boundThis.handler.state = boundThis.attributes.STATE = config.states.ITERATING_EXPLAINER;
-          boundThis.emitWithState('ListExplainers', 'invalid_number');
-        }
-      );
 
-    } else if (!chosenExplainer) {
-      if (slot.query && slot.query.value) {
-        let intentCheck = util.intentCheck(slot.query.value);
-        let externalCheck = util.externalCheck(slot.query.value);
-        if (externalCheck) {
-          this.attributes.EXTERNALS = this.attributes.EXTERNALS || 0;
-          this.attributes.EXTERNALS++;
-          if (this.attributes.EXTERNALS === 1 || (this.attributes.EXTERNALS % config.externalMessageFrequency === 0)) {
-            return this.emitWithState('AMAZON.StopIntent', config.externalMessage);
-          } else {
-            return this.emitWithState('AMAZON.CancelIntent');
-          }
-        } else if (intentCheck) {
-          console.log("Playing Explainer PickItem intentCheck -- slot.query.value ", slot.query.value)
+    util.asyncExplainers.call(this, function(err, resp) {
+      if(err) {
+        console.log("ASYNC EXPLAINER ERR", err);
+      }
+      var allExplainers = resp;
+
+      var slot = slot || this.event.request.intent.slots;
+      var boundThis = this;
+      let addOneBool = false;
+      if (source === 'HOME_AFTER_LAUNCH') {
+        addOneBool = true
+      }
+      var chosenExplainer = util.itemPicker(slot, allExplainers, 'title', 'topic', addOneBool);
+      if (chosenExplainer === -1) {
+        var theNumber;
+        if (slot.query && slot.query.value) {
+          theNumber = util.stripActions(slot.query.value);
           delete slot.query.value;
-          return this.emitWithState(intentCheck);
+        } else if (slot.index && slot.index.value) {
+          theNumber = util.stripActions(slot.index.value);
+          delete slot.index.value;
+        } else if (slot.ordinal && slot.ordinal.value) {
+          theNumber = util.stripActions(slot.ordinal.value);
+          delete slot.ordinal.value;
         }
-        console.log("PLAYING_EXPLAINER, PickItem - slot.query.value but could not find -- SENDING TO UNRESOLVED");
-        this.handler.state = this.attributes.STATE = config.states.UNRESOLVED;
-        return this.emitWithState('PickItem');
-      } else if (slot.index && slot.index.value) {
-        console.log("NO EXPLAINER FOUND, but there is INDEX ", JSON.stringify(slot, null,2))
-        var message = `${slot.index.value} is not a valid choice. Please choose between 1 and ${util.liveExplainers().length}. Let's try again. `
+        var message = `${theNumber} is not a valid choice. Please choose between 1 and 99. Let's try again. `
         var boundThis = this;
-        return util.sendProgressive(
-          boundThis.event.context.System.apiEndpoint, // no need to add directives params
-          boundThis.event.request.requestId,
-          boundThis.event.context.System.apiAccessToken,
-          message,
-          function (err) {
-            boundThis.handler.state = boundThis.attributes.STATE = config.states.ITERATING_EXPLAINER;
-            boundThis.emitWithState('ListExplainers');
-          }
-        );
-      } else if (slot.ordinal && slot.ordinal.value) {
-        console.log("NO EXPLAINER , but there is ORDINAL ", JSON.stringify(slot, null,2))
-        let theOrdinal = slot.ordinal.value;
-        delete slot.ordinal.value;
-        var message = `We don't have a ${theOrdinal}. Please choose between 1 and ${util.liveExplainers().length}. Let's try again. `
         return util.sendProgressive(
           boundThis.event.context.System.apiEndpoint, // no need to add directives params
           boundThis.event.request.requestId,
@@ -107,100 +62,153 @@ module.exports = Alexa.CreateStateHandler(config.states.PLAYING_EXPLAINER, {
             boundThis.emitWithState('ListExplainers', 'invalid_number');
           }
         );
-      } else if (slot.topic && slot.topic.value) {
-        // convert to query?
-        console.log("PLAYING_EXPLAINER, PickItem - slot.topic.value but could not find -- SENDING TO UNRESOLVED");
-        this.handler.state = this.attributes.STATE = config.states.UNRESOLVED;
-        return this.emitWithState('PickItem', slot);
-      } else {
-        console.log("NO EXPLAINER, and no slot info I can use ", JSON.stringify(slot, null,2));
-        var message = `Sorry, I couldn't quite understand that.`;
-        return util.sendProgressive(
-          boundThis.event.context.System.apiEndpoint, // no need to add directives params
-          boundThis.event.request.requestId,
-          boundThis.event.context.System.apiAccessToken,
-          message,
-          function (err) {
-            return boundThis.emitWithState('HomePage', 'no_welcome')
-          }
-        );
-      }
-    } else {
-      // IF it's EASTER EGG:
-      if (chosenExplainer.EE) {
-        // set THIS.EASTER_EGG to true
-        // set index to 0 just incase
-        this.attributes.EASTER_EGG_TITLE = chosenExplainer.title.toLowerCase();
-      }
-      console.time('PLAY-DB');
-      var payload = {};
-      payload.explainers = [{
-        source: source || 'EXTERNAL',
-        guid: chosenExplainer.guid,
-        timestamp: this.event.request.timestamp,
-      }]
-      this.attributes.indices.explainer = 0; // if we're playing, might as well reset the list
-      db.update.call(this, payload, function(err, resp) {
-        console.timeEnd('PLAY-DB');
-        this.attributes.currentExplainerIndex = chosenExplainer.index;
-        util.logExplainer.call(this, chosenExplainer);
-        var author = chosenExplainer.author;
-        if (author === 'Molly Wood') {
-          author = `Molly <emphasis level="strong"> Wood</emphasis>`;
-        }
-        console.log("SOURCE PICK ", source)
-        var intro = ''
-        if (this.event.session.new) {
-          intro += 'Welcome to Make Me Smart! ';
-        }
-        if (source && source === 'NEW_USER_LAUNCH_PICK') {
-          intro += `<audio src="${config.newUserAudio}" /> `;
-        }
-        if (chosenExplainer.EE) {
-          intro += 'Congratulations, you found a secret easter egg. '
-        }
-        intro += `Here's ${author} explaining ${chosenExplainer.title}`;
-        if (chosenExplainer.requestInformation && chosenExplainer.requestInformation.user) {
-          intro += `, as requested by ${chosenExplainer.requestInformation.user}`;
-          if (chosenExplainer.requestInformation.location) {
-            intro += ` from ${chosenExplainer.requestInformation.location}`
-          }
-        }
-        intro += `. <break time = "200ms"/> <audio src="${chosenExplainer.audio.url}" />`;
-        var prompt;
-        if (chosenExplainer.EE) {
-          prompt = `Thanks for listening! By the way, do us a favor and rate Make Me Smart on the Alexa Skill Store or Mobile App. You can hear what's new, browse all our explainers or submit an idea. What would you like to do?`;
-        } else if (this.event.session.new) { // came directly here
-          prompt = `You can replay the explainer, play the latest, or browse all our explainers. What would you like to do?`;
-        } else if (util.liveExplainers()[chosenExplainer.index+1]) { // THERE IS a next explainer
-          prompt = `You can replay the explainer, say 'next' to hear another, or browse all our explainers. What would you like to do?`;
-        } else { // end of the line
-          prompt = "And that's all we have right now. You can replay the explainer, browse all our explainers, or submit an idea for our next one. What would you like to do?"
-        }
-        let displayMessage;
-        if (chosenExplainer.EE) {
-          displayMessage = "Congrats! You found an easter egg! Thanks for being a power user! Do us a favor and rate Make Me Smart on the Alexa Skill Store or Mobile App!";
-        } else {
-          displayMessage = util.displayMessage.call(this);
 
-        }
-        if (this.event.context.System.device.supportedInterfaces.Display) {
-          this.response.renderTemplate(
-            util.templateBodyTemplate3(
-              chosenExplainer.title,
-              util.imageForExplainer(chosenExplainer),
-              '',
-              displayMessage,
-              config.background.show
-            )
+      } else if (!chosenExplainer) {
+        if (slot.query && slot.query.value) {
+          let intentCheck = util.intentCheck(slot.query.value);
+          let externalCheck = util.externalCheck(slot.query.value);
+          if (externalCheck) {
+            this.attributes.EXTERNALS = this.attributes.EXTERNALS || 0;
+            this.attributes.EXTERNALS++;
+            if (this.attributes.EXTERNALS === 1 || (this.attributes.EXTERNALS % config.externalMessageFrequency === 0)) {
+              return this.emitWithState('AMAZON.StopIntent', config.externalMessage);
+            } else {
+              return this.emitWithState('AMAZON.CancelIntent');
+            }
+          } else if (intentCheck) {
+            console.log("Playing Explainer PickItem intentCheck -- slot.query.value ", slot.query.value)
+            delete slot.query.value;
+            return this.emitWithState(intentCheck);
+          }
+          console.log("PLAYING_EXPLAINER, PickItem - slot.query.value but could not find -- SENDING TO UNRESOLVED");
+          this.handler.state = this.attributes.STATE = config.states.UNRESOLVED;
+          return this.emitWithState('PickItem');
+        } else if (slot.index && slot.index.value) {
+          console.log("NO EXPLAINER FOUND, but there is INDEX ", JSON.stringify(slot, null,2))
+          var message = `${slot.index.value} is not a valid choice. Please choose between 1 and 99. Let's try again. `
+          var boundThis = this;
+          return util.sendProgressive(
+            boundThis.event.context.System.apiEndpoint, // no need to add directives params
+            boundThis.event.request.requestId,
+            boundThis.event.context.System.apiAccessToken,
+            message,
+            function (err) {
+              boundThis.handler.state = boundThis.attributes.STATE = config.states.ITERATING_EXPLAINER;
+              boundThis.emitWithState('ListExplainers');
+            }
+          );
+        } else if (slot.ordinal && slot.ordinal.value) {
+          console.log("NO EXPLAINER , but there is ORDINAL ", JSON.stringify(slot, null,2))
+          let theOrdinal = slot.ordinal.value;
+          delete slot.ordinal.value;
+          var message = `We don't have a ${theOrdinal}. Please choose between 1 and 99. Let's try again. `
+          return util.sendProgressive(
+            boundThis.event.context.System.apiEndpoint, // no need to add directives params
+            boundThis.event.request.requestId,
+            boundThis.event.context.System.apiAccessToken,
+            message,
+            function (err) {
+              boundThis.handler.state = boundThis.attributes.STATE = config.states.ITERATING_EXPLAINER;
+              boundThis.emitWithState('ListExplainers', 'invalid_number');
+            }
+          );
+        } else if (slot.topic && slot.topic.value) {
+          // convert to query?
+          console.log("PLAYING_EXPLAINER, PickItem - slot.topic.value but could not find -- SENDING TO UNRESOLVED");
+          this.handler.state = this.attributes.STATE = config.states.UNRESOLVED;
+          return this.emitWithState('PickItem', slot);
+        } else {
+          console.log("NO EXPLAINER, and no slot info I can use ", JSON.stringify(slot, null,2));
+          var message = `Sorry, I couldn't quite understand that.`;
+          return util.sendProgressive(
+            boundThis.event.context.System.apiEndpoint, // no need to add directives params
+            boundThis.event.request.requestId,
+            boundThis.event.context.System.apiAccessToken,
+            message,
+            function (err) {
+              return boundThis.emitWithState('HomePage', 'no_welcome')
+            }
           );
         }
-        var fullSpeech = intro + prompt;
-        this.response.speak(fullSpeech).listen(prompt);
-        this.emit(':saveState', true);
-      });
+      } else {
+        // IF it's EASTER EGG:
+        if (chosenExplainer.EE) {
+          // set THIS.EASTER_EGG to true
+          // set index to 0 just incase
+          this.attributes.EASTER_EGG_TITLE = chosenExplainer.title.toLowerCase();
+        }
+        console.time('PLAY-DB');
+        var payload = {};
+        payload.explainers = [{
+          source: source || 'EXTERNAL',
+          guid: chosenExplainer.guid,
+          timestamp: this.event.request.timestamp,
+        }]
+        this.attributes.indices.explainer = 0; // if we're playing, might as well reset the list
+        db.update.call(this, payload, function(err, resp) {
+          console.timeEnd('PLAY-DB');
+          this.attributes.currentExplainerIndex = chosenExplainer.index;
+          util.logExplainer.call(this, chosenExplainer);
+          var author = chosenExplainer.author;
+          if (author === 'Molly Wood') {
+            author = `Molly <emphasis level="strong"> Wood</emphasis>`;
+          }
+          console.log("SOURCE PICK ", source)
+          var intro = ''
+          if (this.event.session.new) {
+            intro += 'Welcome to Make Me Smart! ';
+          }
+          if (source && source === 'NEW_USER_LAUNCH_PICK') {
+            intro += `<audio src="${config.newUserAudio}" /> `;
+          }
+          if (chosenExplainer.EE) {
+            intro += 'Congratulations, you found a secret easter egg. '
+          }
+          intro += `Here's ${author} explaining ${chosenExplainer.title}`;
+          if (chosenExplainer.requestInformation && chosenExplainer.requestInformation.user) {
+            intro += `, as requested by ${chosenExplainer.requestInformation.user}`;
+            if (chosenExplainer.requestInformation.location) {
+              intro += ` from ${chosenExplainer.requestInformation.location}`
+            }
+          }
+          intro += `. <break time = "200ms"/> <audio src="${chosenExplainer.audio.url}" />`;
+          var prompt;
+          if (chosenExplainer.EE) {
+            prompt = `Thanks for listening! By the way, do us a favor and rate Make Me Smart on the Alexa Skill Store or Mobile App. You can hear what's new, browse all our explainers or submit an idea. What would you like to do?`;
+          } else if (this.event.session.new) { // came directly here
+            prompt = `You can replay the explainer, play the latest, or browse all our explainers. What would you like to do?`;
+          } else if (allExplainers[chosenExplainer.index+1]) { // THERE IS a next explainer
+            prompt = `You can replay the explainer, say 'next' to hear another, or browse all our explainers. What would you like to do?`;
+          } else { // end of the line
+            prompt = "And that's all we have right now. You can replay the explainer, browse all our explainers, or submit an idea for our next one. What would you like to do?"
+          }
+          let displayMessage;
+          if (chosenExplainer.EE) {
+            displayMessage = "Congrats! You found an easter egg! Thanks for being a power user! Do us a favor and rate Make Me Smart on the Alexa Skill Store or Mobile App!";
+          } else {
+            displayMessage = util.displayMessage.call(this);
 
-    }
+          }
+          if (this.event.context.System.device.supportedInterfaces.Display) {
+            this.response.renderTemplate(
+              util.templateBodyTemplate3(
+                chosenExplainer.title,
+                util.imageForExplainer(chosenExplainer),
+                '',
+                displayMessage,
+                config.background.show
+              )
+            );
+          }
+          var fullSpeech = intro + prompt;
+          this.response.speak(fullSpeech).listen(prompt);
+          this.emit(':saveState', true);
+        });
+
+      }
+    }) // close async explainer call
+
+    // end of function
   },
   'PlayLatestExplainer': function () {
     delete this.attributes.EASTER_EGG_TITLE
@@ -308,7 +316,7 @@ module.exports = Alexa.CreateStateHandler(config.states.PLAYING_EXPLAINER, {
     console.log("NEXT!!!! EXPLAINER", this.handler.state)
     var deviceId = util.getDeviceId.call(this);
     util.nullCheck.call(this, deviceId);
-    if (util.liveExplainers().length <= this.attributes.currentExplainerIndex +1) {
+    if (100 <= this.attributes.currentExplainerIndex +1) {
       // last spot
       var message = "We don't have any more explainers right now. You can browse all our explainers or hear what's new for the latest. What would you like to do?"
       var prompt = "You can browse all our explainers or hear what's new for the latest. What would you like to do?"
